aa894612e24db0d11aa29662d53be74f375e9ea8
joel <joelfiddes@gmail.com>
2017-10-18 23:56:02 +0200

    clean up ensembleRun.py

--- ensembleRun.py
+++ ensembleRun.py
@@ -4,50 +4,19 @@ import pandas as pd
 import time
 import sys
 
-#set root
-root = "/home/joel/sim/ensembler_scale_sml/"
-# number of ensemble memebers
-N = 50
+#===============================================================================================
+# PARAMETERS TO SET
+
+root = "/home/joel/sim/ensembler_testRadflux/"
+inifile="test.ini"
+initdir='/home/joel/sim/test_radflux'
+initgrid = "2"
+N = 100# number of ensemble memebers
+#===============================================================================================
+
+
 # Timer
 start_time = time.time()
-
-#ensure exists
-os.system("python writeConfig.py")
-
-# Do norm run - doesnt work as needs different config settings (initSim = FALSE)
-# if len(sys.argv) == 2 and sys.argv[1] == 'NORM':
-# 	print "Running NORMAL simulation"
-# 	pbias = 1
-# 	tbias = 0
-
-# 	from configobj import ConfigObj
-# 	config = ConfigObj("topomap.ini")
-# 	config.filename = "topomap.ini"
-# 	config["main"]["wd"]  = root + "ensemble_norm/"
-# 	config["main"]["initSim"]  = 'TRUE'
-# 	config["da"]["pscale"] = pbias #factor to multiply precip by
-# 	config["da"]["tscale"] = tbias #factor to add to temp
-# 	config['modis']['getMODISSCA'] = "TRUE"
-# 	config.write()
-
-# 	print "[INFO]: Config settings used"
-# 	print(config)
-
-# 	print "[INFO]: Running TopoMap"
-# 	os.system("python code_da.py")
-
-# 	print("[INFO]: %f minutes for run of NORM" % round((time.time()/60 - start_time/60),2))
-
-
-# check norm exists
-# if os.path.isdir(root + "/ensemble_norm") == False:
-# 	print "No ensemble_norm, run <ensembleRun.py NORM> to generate.. ABORTING"
-# 	exit()
-# elif os.path.isdir(root + "ensemble_norm") == True:
-# 	print "ensemble_norm found"	
-
-
-
 
 
 # start ensemble runs
@@ -72,23 +41,25 @@ for i in range(0,N):
 
 	print "[INFO]: Config ensemble members"
 	from configobj import ConfigObj
-	config = ConfigObj("topomap.ini")
-	config.filename = "topomap.ini"
+	config = ConfigObj(inifile)
+	config.filename = inifile
 	config["main"]["wd"]  = root + "ensemble" + str(i) + "/"
 	config["da"]["pscale"] = pbias #factor to multiply precip by
 	config["da"]["tscale"] = tbias #factor to add to temp
+	config["da"]["swscale"] = swbias
+	config["da"]["lwscale"] = lwbias
 	config['modis']['getMODISSCA'] = "FALSE"
 	config["main"]["initSim"]  = 'TRUE'
-	config['main']['initDir'] = '/home/joel/sim/scale_test_smlkk'
+	config['main']['initDir'] = initdir
 	config['toposub']['inform'] = 'FALSE'
-	config['main']['initGrid'] = "9"
+	config['main']['initGrid'] = initgrid
 	config.write()
 
 	print "[INFO]: Config settings used"
 	print(config)
 
-	print "[INFO]: Running code_da.py"
-	os.system("python code_da.py")
+	print "[INFO]: Running topomapp_main.py"
+	os.system("python topomapp_main.py " + inifile)
 
 
 print("[INFO]: %f minutes for run of ensemble members" % round((time.time()/60 - start_time/60),2))
