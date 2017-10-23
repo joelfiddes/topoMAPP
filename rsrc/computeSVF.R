715739384855fe5d9d228097799d17be46045eb6
joel <joelfiddes@gmail.com>
2017-10-23 23:34:52 +0200

    moved svf to seperate module within Ngrid loop to improve scalability (as done for surface calc)

--- rsrc/computeSVF.R
+++ rsrc/computeSVF.R
@@ -5,9 +5,8 @@ dist = as.numeric(args[3])
 
 require(horizon)
 
-if(!file.exists(paste0(gridpath, "/predictors/svf.tif"))){
+#if(!file.exists(paste0(gridpath, "/predictors/svf.tif"))){
 	ele=raster(paste0(gridpath, "/predictors/ele.tif"))
 	s <- svf(ele, nAngles=6, maxDist=500, ll=TRUE)
-	setwd(paste0(gridpath,'/predictors'))
 	writeRaster(round(s,2), paste0(gridpath, "/predictors/svf.tif"), overwrite=TRUE) #write and reduce precision
-	}
+	#}
