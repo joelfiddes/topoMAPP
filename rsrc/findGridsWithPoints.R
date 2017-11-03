98caf30a73de35608a2111e85eae31bc053a2e8a
joel <joelfiddes@gmail.com>
2017-11-04 00:56:33 +0100

    find grids without points

--- rsrc/findGridsWithPoints.R
+++ rsrc/findGridsWithPoints.R
@@ -3,8 +3,8 @@
 require(raster)
 
 args = commandArgs(trailingOnly=TRUE)
-rst <- arg[1] # raster("/home/joel/sim/test_points/spatial/eraExtent.tif")
-shp  <- arg[2] # shapefile("/home/joel/data/imis/gis/IMIS_GR.shp")
+rst <- args[1] # raster("/home/joel/sim/test_points/spatial/eraExtent.tif")
+shp  <- args[2] # shapefile("/home/joel/data/imis/gis/IMIS_GR.shp")
 
 # replace values 1: ncell this is the grids index
 values(rst)<- 1:ncell(rst)
