7417170f70615acd3d309336d7720370c3e0c3e8
joel <joelfiddes@gmail.com>
2017-11-06 10:30:30 +0100

    remove strange matrix multiplication in tscale_lw.R

--- rsrc/getMODIS_SCA.R
+++ rsrc/getMODIS_SCA.R
@@ -1,8 +1,10 @@
+modisDescript <- packageDescription('MODIStsp')
+print(modisDescript)
+
 require(MODIStsp) 
 
 args = commandArgs(trailingOnly=TRUE)
 gui=args[1]
 options_file=args[2]
-modisDescript <- packageDescription('MODIStsp')
-print(modisDescript)
+
 MODIStsp(gui = gui, options_file = options_file) 
