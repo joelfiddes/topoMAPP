eb53b2b3301cbfb6cc6b13bb1696b2e59ebbaa35
joel <joelfiddes@gmail.com>
2018-04-15 23:40:04 +0500

    getGridEle.R  computed grid elebased on original downloaded era extent which was buffered by 1 grid cell in all directions. This had main effect on precip which is scaled according eleDiff. Error caused more than factor 2 differences in PSUM. Now computing based on cropped extent eraExtent.tif Affecte all sims since getGridEle.R created on 12 Oct 2017

--- rsrc/getGridEle.R
+++ rsrc/getGridEle.R
@@ -5,6 +5,7 @@
 
 #DEPENDENCY
 require(ncdf4)
+require(raster)
 #SOURCE
 source('./rsrc/tscale_src.R')
 #====================================================================
@@ -17,14 +18,25 @@ wd=args[1]
 # PARAMETERS FIXED
 #====================================================================
 file = paste0(wd,'/eraDat/SURF.nc')
+eraExtent = raster(paste0(wd,"/spatial/eraExtent.tif"))
+
+#get extent
+r=raster(file)
 #**********************  SCRIPT BEGIN *******************************
 setwd(wd)
 nc=nc_open(file)
 gp = ncvar_get( nc,'z')
 surfEle = gp[,, 1] / 9.80665
 
-ele=as.vector(surfEle)
-grid=1:length(surfEle)
+# assign these values to containt "r"
+r2 =setValues(r, as.vector(surfEle))
+
+# crop by actual extent
+r3 = crop(r2, eraExtent)
+ele = getValues(r3)
+
+#ele=as.vector(surfEle)
+grid=1:length(ele)
 
 # construct dataframe
 df=data.frame(grid, ele)
